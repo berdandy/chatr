1ebc612e0f3e03ed9c67d9f6c37f840309249344
Andy Berdan <andy@berdan.ca>
2022-04-04 15:21:12 -0400

    Added base64 decode sample code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # chatr
+
 Transform GW2 Template Codes into something else
+
+# TODO:
+
+- [ ] figure out how to make an api call from rust
+- [ ] figure out how to unpack base64 string to struct from Anet
+- [ ] translate struct into IDs
+- [ ] format IDs into armory-embeds output template
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::str;
 use std::env;
 use std::process;
 
@@ -9,35 +10,8 @@ fn main() {
     }
 
     let chatcode = &args[1];
-
     println!("Deciphering {}", chatcode);
+
+    let decoded = base64::decode(chatcode).unwrap();
+    println!("{:?}", str::from_utf8(&decoded));
 }
-
-
-/* OLD EXAMPLE IGNORE
- *use std::fs::File;
-use std::io::{BufReader, BufRead, Error};
-
-fn main() -> Result<(), Error> {
-    let path = "test.txt";
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
-
-    let right = 3;
-    let down = 1;
-
-    let mut trees_hit = 0;
-
-    for (i, line) in buffered.lines().step_by(down).enumerate()
-    {
-        let line_data: String = line.unwrap();
-        if line_data.chars().nth((i * right) % line_data.len()).unwrap() == '#'
-        {
-            trees_hit += 1;
-        }
-    }
-    println!("{}", trees_hit);
-
-    Ok(())
-}
-*/
