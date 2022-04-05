423350efb0743f6d25efe5906501ac904206f407
Andy Berdan <andy@berdan.ca>
2022-04-05 19:57:09 -0400

    Added trait index lookup

---
+++ src/build_template_reference.cpp
@@ -0,0 +1,86 @@
+// C++ format provided by ArenaNet: https://en-forum.guildwars2.com/topic/65255-api-updates-december-18-2019/
+struct BuildChatLink
+{
+    std::uint8_t magic = 0xd;
+    // Profession codes can be found in the `code` field at
+    // https://api.guildwars2.com/professions
+    std::uint8_t profession;
+    // Specializations can be found at
+    // https://api.guildwars2.com/specializations/:id
+    // To find a trait ID given a specialization, first find the `major_traits`
+    // array at https://api.guildwars2.com/v2/specializations/:id
+    // Then, reference this table to find the trait ID:
+    //        field        | value |    trait ID
+    // ====================+=======+=================
+    // traitAdeptN         |     0 | None
+    //                     |     1 | major_traits[0]
+    //                     |     2 | major_traits[1]
+    //                     |     3 | major_traits[2]
+    // traitMasterN        |     0 | None
+    //                     |     1 | major_traits[3]
+    //                     |     2 | major_traits[4]
+    //                     |     3 | major_traits[5]
+    // traitGrandmasterN   |     0 | None
+    //                     |     1 | major_traits[6]
+    //                     |     2 | major_traits[7]
+    //                     |     3 | major_traits[8]
+    // Traits themselves can be found at https://api.guildwars2.com/traits/:id
+    std::uint8_t specialization1;
+    std::uint8_t traitAdept1 : 2;
+    std::uint8_t traitMaster1 : 2;
+    std::uint8_t traitGrandmaster1 : 2;
+    std::uint8_t traitPadding1 : 2;
+    std::uint8_t specialization2;
+    std::uint8_t traitAdept2 : 2;
+    std::uint8_t traitMaster2 : 2;
+    std::uint8_t traitGrandmaster2 : 2;
+    std::uint8_t traitPadding2 : 2;
+    std::uint8_t specialization3;
+    std::uint8_t traitAdept3 : 2;
+    std::uint8_t traitMaster3 : 2;
+    std::uint8_t traitGrandmaster3 : 2;
+    std::uint8_t traitPadding3 : 2;
+    // To find a skill ID given a palette ID, first find the `skills_by_palette` array
+    // at https://api.guildwars2.com/v2/professions/:id for this build's profession.
+    // Then, find the pair with the palette ID and use the mapped value.
+    // For example, in javascript you might look it up like so:
+    //     new Map(skills_by_palette).get(paletteID)
+    // Skills themselves can be found at https://api.guildwars2.com/v2/skills/:id
+    std::uint16_t terrestrialHealingSkillPalette;
+    std::uint16_t aquaticHealingSkillPalette;
+    std::uint16_t terrestrialUtilitySkillPalette1;
+    std::uint16_t aquaticUtilitySkillPalette1;
+    std::uint16_t terrestrialUtilitySkillPalette2;
+    std::uint16_t aquaticUtilitySkillPalette2;
+    std::uint16_t terrestrialUtilitySkillPalette3;
+    std::uint16_t aquaticUtilitySkillPalette3;
+    std::uint16_t terrestrialEliteSkillPalette;
+    std::uint16_t aquaticEliteSkillPalette;
+    union
+    {
+        struct
+        {
+            // Pets can be found at
+            // https://api.guildwars2.com/v2/pets/:id
+            std::uint8_t terrestrialPet1;
+            std::uint8_t terrestrialPet2;
+            std::uint8_t aquaticPet1;
+            std::uint8_t aquaticPet2;
+        } ranger;
+        struct
+        {
+            // Legend codes can be found in the `code` field at
+            // https://api.guildwars2.com/v2/legends/:id
+            std::uint8_t activeTerriestralLegend;
+            std::uint8_t inactiveTerrestrialLegend;
+            std::uint8_t activeAquaticLegend;
+            std::uint8_t inactiveAquaticLegend;
+            std::uint16_t inactiveTerrestrialLegendUtilitySkillPalette1;
+            std::uint16_t inactiveTerrestrialLegendUtilitySkillPalette2;
+            std::uint16_t inactiveTerrestrialLegendUtilitySkillPalette3;
+            std::uint16_t inactiveAquaticLegendUtilitySkillPalette1;
+            std::uint16_t inactiveAquaticLegendUtilitySkillPalette2;
+            std::uint16_t inactiveAquaticLegendUtilitySkillPalette3;
+        } revenant;
+    };
+};
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,56 @@
-use std::str;
 use std::env;
 use std::process;
+use std::collections::HashMap;
+
+use deku::prelude::*;
+
+// see cpp reference doc
+#[derive(Debug, PartialEq, DekuRead, DekuWrite)]
+pub struct BuildTemplate {
+    magic: u8,              // must be 0xD
+    profession: u8,         // 0-based, IDs on API are 1-based (but not used by armory-embeds, so w/e)
+
+    specialization1: u8,
+    #[deku(bits = "2")]
+    trait_padding_1: u8,
+    #[deku(bits = "2")]
+    trait_grandmaster_1: u8,
+    #[deku(bits = "2")]
+    trait_master_1: u8,
+    #[deku(bits = "2")]
+    trait_adept_1: u8,
+
+    specialization2: u8,
+    #[deku(bits = "2")]
+    trait_padding_2: u8,
+    #[deku(bits = "2")]
+    trait_grandmaster_2: u8,
+    #[deku(bits = "2")]
+    trait_master_2: u8,
+    #[deku(bits = "2")]
+    trait_adept_2: u8,
+
+    specialization3: u8,
+    #[deku(bits = "2")]
+    trait_padding_3: u8,
+    #[deku(bits = "2")]
+    trait_grandmaster_3: u8,
+    #[deku(bits = "2")]
+    trait_master_3: u8,
+    #[deku(bits = "2")]
+    trait_adept_3: u8,
+
+    terrestrial_healing_skill_palette: u16,
+    aquatic_healing_skill_palette: u16,
+    terrestrial_utility_skill_palette_1: u16,
+    aquatic_utility_skill_palette_1: u16,
+    terrestrial_utility_skill_palette_2: u16,
+    aquatic_utility_skill_palette_2: u16,
+    terrestrial_utility_skill_palette_3: u16,
+    aquatic_utility_skill_palette_3: u16,
+    terrestrial_elite_skill_palette: u16,
+    aquatic_elite_skill_palette: u16,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,6 +62,25 @@ fn main() {
     let chatcode = &args[1];
     println!("Deciphering {}", chatcode);
 
-    let decoded = base64::decode(chatcode).unwrap();
-    println!("{:?}", str::from_utf8(&decoded));
+    let data = base64::decode(chatcode)
+        .expect("invaid base64");
+
+    let (_rest, build) = BuildTemplate::from_bytes((data.as_ref(), 0)).unwrap();
+
+    println!("{:?}", build);
+
+    // hardcoded for testing, but this should hit the GW2 API at https://api.guildwars2.com/v2/specializations/<id>
+    let trait_ids_by_specialization = HashMap::from([
+        (1, [701, 705, 700, 1889, 1960, 708, 692, 1950, 704]),      // mesmer - dueling
+        (6, [514, 525, 1882, 482, 1892, 1944, 1541, 505, 1947]),    // engineer - explosives
+    ]);
+
+    if trait_ids_by_specialization.contains_key(&build.specialization1)
+    {
+        let trait_ids = trait_ids_by_specialization[&build.specialization1];
+        let trait1 = trait_ids[(build.trait_adept_1 - 1) as usize];
+        let trait2 = trait_ids[(build.trait_master_1 + 3 - 1) as usize];
+        let trait3 = trait_ids[(build.trait_grandmaster_1 + 6 - 1) as usize];
+        println!("Traits: {} {} {}", trait1, trait2, trait3);
+    }
 }
